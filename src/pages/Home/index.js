cfa8b2c8e867e014561e61b41fc5ad4ac7ac71f9
milena-mognon <milenamognon@gmail.com>
2020-03-27 20:27:43 -0300

    Cart styles, API configuration

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,85 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { MdAddShoppingCart } from 'react-icons/md';
 import { ProductList } from './styles';
+import api from '../../services/api';
+import { formatPrice } from '../../util/format';
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-exact-masculino/06/D22-2020-006/D22-2020-006_zoom1.jpg?ts=1572880927&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis Muito Legal</strong>
-        <span>R$159,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+  async componentDidMount() {
+    const response = await api.get('/products');
 
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-exact-masculino/06/D22-2020-006/D22-2020-006_zoom1.jpg?ts=1572880927&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis Muito Legal</strong>
-        <span>R$159,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
 
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-exact-masculino/06/D22-2020-006/D22-2020-006_zoom1.jpg?ts=1572880927&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis Muito Legal</strong>
-        <span>R$159,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+    this.setState({ products: data });
+  }
 
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-exact-masculino/06/D22-2020-006/D22-2020-006_zoom1.jpg?ts=1572880927&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis Muito Legal</strong>
-        <span>R$159,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-exact-masculino/06/D22-2020-006/D22-2020-006_zoom1.jpg?ts=1572880927&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis Muito Legal</strong>
-        <span>R$159,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+  render() {
+    const { products } = this.state;
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" />3
+              </div>
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
