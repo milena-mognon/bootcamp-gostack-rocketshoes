b6fe958e17d9f2b38da510e50b997d2f26cf5827
milena-mognon <milenamognon@gmail.com>
2020-03-26 21:44:48 -0300

    Home styles

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,85 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-exact-masculino/06/D22-2020-006/D22-2020-006_zoom1.jpg?ts=1572880927&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis Muito Legal</strong>
+        <span>R$159,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-exact-masculino/06/D22-2020-006/D22-2020-006_zoom1.jpg?ts=1572880927&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis Muito Legal</strong>
+        <span>R$159,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-exact-masculino/06/D22-2020-006/D22-2020-006_zoom1.jpg?ts=1572880927&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis Muito Legal</strong>
+        <span>R$159,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-exact-masculino/06/D22-2020-006/D22-2020-006_zoom1.jpg?ts=1572880927&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis Muito Legal</strong>
+        <span>R$159,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-exact-masculino/06/D22-2020-006/D22-2020-006_zoom1.jpg?ts=1572880927&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis Muito Legal</strong>
+        <span>R$159,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
